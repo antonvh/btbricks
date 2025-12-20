803b07ca675caf3d7eea9f854d086a2ed6a7d613
Anton Vanhoucke <antonvh@gmail.com>
2025-12-20 15:45:51 +0100

    bump version and fix setup.py

--- setup.py
+++ setup.py
@@ -27,50 +27,68 @@ pip install btbricks
 """
 
 from setuptools import setup, find_packages
+import pathlib
 
+# Read long description from README
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
+# Read metadata from pyproject.toml so we only need to update one file
+pyproject_path = pathlib.Path("pyproject.toml")
+pyproject_data = {}
+if pyproject_path.exists():
+    try:
+        # Python 3.11+
+        import tomllib as _toml
+
+        pyproject_data = _toml.loads(pyproject_path.read_text(encoding="utf-8"))
+    except Exception:
+        try:
+            import toml as _toml  # type: ignore
+
+            pyproject_data = _toml.loads(pyproject_path.read_text(encoding="utf-8"))
+        except Exception:
+            pyproject_data = {}
+
+project_meta = pyproject_data.get("project", {}) if isinstance(pyproject_data, dict) else {}
+
+
+def _first_author(meta):
+    authors = meta.get("authors") or []
+    if authors and isinstance(authors, list):
+        first = authors[0]
+        return first.get("name"), first.get("email")
+    return (None, None)
+
+
+author_name, author_email = _first_author(project_meta)
+
+# Fallbacks for commonly used fields
+name = project_meta.get("name", "btbricks")
+version = project_meta.get("version", "0.0.0")
+description = project_meta.get("description", "")
+keywords = ", ".join(project_meta.get("keywords", []))
+classifiers = project_meta.get("classifiers", [])
+python_requires = project_meta.get("requires-python", project_meta.get("python_requires", ">=3.7"))
+install_requires = project_meta.get("dependencies", [])
+extras_require = project_meta.get("optional-dependencies", {})
+project_urls = project_meta.get("urls", {})
+url = (project_urls or {}).get("Homepage", project_meta.get("homepage", ""))
+
 setup(
-    name="btbricks",
-    version="0.1.0",
-    author="Anton Vanhoucke",
-    author_email="anton@antonsmindstorms.com",
-    description="A MicroPython Bluetooth library for remote controlling LEGO hubs via BLE",
+    name=name,
+    version=version,
+    author=author_name or "",
+    author_email=author_email or "",
+    description=description,
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/antonvh/btbricks",
+    url=url,
     packages=find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: Implementation :: MicroPython",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Intended Audience :: Developers",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: System :: Hardware",
-        "Environment :: Handhelds/PDA Devices",
-    ],
-    python_requires=">=3.7",
-    install_requires=[],
-    extras_require={
-        "dev": [
-            "pytest>=7.0",
-            "pytest-asyncio>=0.20.0",
-            "black>=23.0",
-            "flake8>=5.0",
-            "mypy>=1.0",
-        ],
-    },
-    keywords="micropython bluetooth ble lego hub control",
-    project_urls={
-        "Bug Reports": "https://github.com/antonvh/btbricks/issues",
-        "Source": "https://github.com/antonvh/btbricks",
-        "Documentation": "https://btbricks.readthedocs.io",
-    },
+    classifiers=classifiers,
+    python_requires=python_requires,
+    install_requires=install_requires,
+    extras_require=extras_require,
+    keywords=keywords,
+    project_urls=project_urls,
 )
